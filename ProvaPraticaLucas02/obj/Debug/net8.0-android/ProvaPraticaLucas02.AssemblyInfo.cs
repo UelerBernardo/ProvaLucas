7e16751227cb793bef0f15d0d288c711f8571f64
ueler <uelerccb1698@gmail.com>
2025-10-07 18:51:14 -0300

    Agora vai

--- ProvaPraticaLucas02/obj/Debug/net8.0-android/ProvaPraticaLucas02.AssemblyInfo.cs
+++ ProvaPraticaLucas02/obj/Debug/net8.0-android/ProvaPraticaLucas02.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProvaPraticaLucas02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0b78bb1a354344248ddf39469cafbbd85722ff27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0ecdb8dc1b252968b7ed4539ab7d70c2be3c64d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProvaPraticaLucas02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProvaPraticaLucas02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
